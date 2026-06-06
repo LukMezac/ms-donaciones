204fb561a206e7fdfb0187286740db36fbf6b666
LukMezac <luk.mezac@duocuc.cl>
2026-06-06 17:43:24 -0400

    feat: agregar documentación de API con Swagger/OpenAPI

--- src/test/java/com/donaton/donacion/MsDonacionesApplicationTests.java
+++ src/test/java/com/donaton/donacion/MsDonacionesApplicationTests.java
@@ -1,11 +1,14 @@
 package com.donaton.donacion;
 
 import com.donaton.donaciones.MsDonacionesApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testng.annotations.Test;
 
-@SpringBootTest(classes = MsDonacionesApplication.class) // Importa la clase principal aquí
+
+@AutoConfigureMockMvc // Si usas MockMvc
+// Excluye la configuración automática de la BD para los tests unitarios
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class MsDonacionesApplicationTests {
-	@Test
-	void contextLoads() { }
+	// ...
 }
