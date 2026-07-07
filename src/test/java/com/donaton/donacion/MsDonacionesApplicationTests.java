e2d73cd0ec3bc8fbad06fdf621109545e8e65522
LukMezac <luk.mezac@duocuc.cl>
2026-07-07 04:33:50 -0400

    test: agregando pruebas unitarias para DonacionController con MockMvc

--- src/test/java/com/donaton/donacion/MsDonacionesApplicationTests.java
+++ src/test/java/com/donaton/donacion/MsDonacionesApplicationTests.java
@@ -1,14 +1,16 @@
 package com.donaton.donacion;
 
-import com.donaton.donaciones.MsDonacionesApplication;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testng.annotations.Test;
 
-
-@AutoConfigureMockMvc // Si usas MockMvc
-// Excluye la configuración automática de la BD para los tests unitarios
+@AutoConfigureMockMvc
 @SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class MsDonacionesApplicationTests {
-	// ...
+
+    @Test
+    void contextLoads() {
+
+    }
 }
